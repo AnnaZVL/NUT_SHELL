40ae48889a241a6240ca226b41c8acb5ae5a5685
Anna Zadiraka <annazadiraka@gmail.com>
2023-07-28 18:17:18 +0300

    chenge main, added footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Слайдеры
 const swiperCoffe = new Swiper('.coffee-house__swiper', {
     slidesPerView: 2,
     spaceBetween: 32,
@@ -11,13 +12,71 @@ const swiperOffice = new Swiper('.office__swiper', {
     slidesPerView: 2,
     spaceBetween: 32,
     loop: true,
-    // autoplay: {
-    //     delay: 1000,
-    //   },
+    autoplay: {
+        delay: 1000,
+      },
     pagination: {
         el: ".office-pagination",
         type: "bullets",
         clickable: true,
         dynamicBullets: true,
         },
-})
+});
+
+const swiperCommentsTop = new Swiper('.comments-top__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+        nextEl: '.comments-top-button-next',
+        prevEl: '.comments-top-button-prev',
+    },
+    breakpoints: {
+        990: {
+            slidesPerView: 3,
+            spaceBetween:64
+        },
+        568: {
+            slidesPerView: 2,
+            spaceBetween:64
+        }
+    }
+});
+
+const swiperCommentsBottom = new Swiper('.comments-bottom__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+        nextEl: '.comments-bottom-button-next',
+        prevEl: '.comments-bottom-button-prev',
+    },
+    breakpoints: {
+        990: {
+            slidesPerView: 3,
+            spaceBetween:64
+        },
+        568: {
+            slidesPerView: 2,
+            spaceBetween:64
+        }
+    }
+});
+
+//Аккордион секции вопросов
+new Accordion('.questions__acc');
+
+//Выпадающий текст секцииО нас
+const $btnMore = document.querySelector('.about__btn'),
+    $textMore = document.querySelector('.about__btn-text');
+
+$btnMore.addEventListener('click', () => {
+    $btnMore.classList.toggle('open');
+    $textMore.classList.toggle('open');
+});
+
+//Робота с формой
+const $form = document.querySelector('.form');
+$form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
