355b5de6d152674e12d2368a1f31a97093e59bae
Anna Zadiraka <annazadiraka@gmail.com>
2023-07-30 16:14:11 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
+//import JustValidate from './just-validate';
+
 //Слайдеры
 const swiperCoffe = new Swiper('.coffee-house__swiper', {
-    slidesPerView: 2,
-    spaceBetween: 32,
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
     navigation: {
         nextEl: '.coffee-house-button-next',
         prevEl: '.coffee-house-button-prev',
     },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+            spaceBetween: 32,
+        }}
 })
 
 const swiperOffice = new Swiper('.office__swiper', {
-    slidesPerView: 2,
-    spaceBetween: 32,
+    slidesPerView: 1,
+    spaceBetween: 15,
     loop: true,
-    autoplay: {
-        delay: 1000,
-      },
+    // autoplay: {
+    //     delay: 1000,
+    //   },
     pagination: {
         el: ".office-pagination",
         type: "bullets",
         clickable: true,
         dynamicBullets: true,
         },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+            spaceBetween: 32,
+        }}
 });
 
 const swiperCommentsTop = new Swiper('.comments-top__swiper', {
@@ -32,7 +45,7 @@ const swiperCommentsTop = new Swiper('.comments-top__swiper', {
         prevEl: '.comments-top-button-prev',
     },
     breakpoints: {
-        990: {
+        1024: {
             slidesPerView: 3,
             spaceBetween:64
         },
@@ -52,9 +65,9 @@ const swiperCommentsBottom = new Swiper('.comments-bottom__swiper', {
         prevEl: '.comments-bottom-button-prev',
     },
     breakpoints: {
-        990: {
+        1024: {
             slidesPerView: 3,
-            spaceBetween:64
+            spaceBetween:50
         },
         568: {
             slidesPerView: 2,
@@ -76,7 +89,29 @@ $btnMore.addEventListener('click', () => {
 });
 
 //Робота с формой
+const validate = new window.JustValidate('.form');
+
+validate
+  .addField('#name', [
+    {
+        rule: 'required',
+        errorMessage: 'Field is required',
+    },
+  ])
+  .addField('#phone', [
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },    
+  ]);
+
 const $form = document.querySelector('.form');
+
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
+    document.querySelectorAll('.form__input').forEach(el => {
+        if (el.classList.contains('just-validate-error-field')) {
+            el.classList.add('error');
+        }
+    })
 });
